a8f3fefc653c5e5d490cc029e72506235837de26
sergiotravassos <sergiotravassos@outlook.com>
2022-07-21 13:14:47 +0100

    projeto criado

---
+++ src/test/java/com/sergiotravassos/apispring/ApispringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiotravassos.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
