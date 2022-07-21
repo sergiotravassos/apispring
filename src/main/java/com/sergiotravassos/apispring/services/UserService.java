923c37aef90689bd5688f9866a04309da34dddd8
sergiotravassos <sergiotravassos@outlook.com>
2022-07-21 14:39:10 +0100

    Service layer, component registration

---
+++ src/main/java/com/sergiotravassos/apispring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.sergiotravassos.apispring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sergiotravassos.apispring.entities.User;
+import com.sergiotravassos.apispring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
