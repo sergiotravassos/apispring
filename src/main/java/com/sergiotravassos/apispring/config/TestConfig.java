92b631ad5975bd9fee7274321f2df0b2373c5431
sergiotravassos <sergiotravassos@outlook.com>
2022-07-25 16:02:14 +0100

    Payment, one-to-one association

--- src/main/java/com/sergiotravassos/apispring/config/TestConfig.java
+++ src/main/java/com/sergiotravassos/apispring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sergiotravassos.apispring.entities.Category;
 import com.sergiotravassos.apispring.entities.Order;
 import com.sergiotravassos.apispring.entities.OrderItem;
+import com.sergiotravassos.apispring.entities.Payment;
 import com.sergiotravassos.apispring.entities.Product;
 import com.sergiotravassos.apispring.entities.User;
 import com.sergiotravassos.apispring.entities.enums.OrderStatus;
@@ -79,6 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 }
